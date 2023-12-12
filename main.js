cc169c4d5d1e018ce03b4a0471e5cc273a1de414
ScxttM <alan.montesinos@uabc.edu.mx>
2023-12-12 13:23:05 -0800

    proyecto terminado

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
+function selectImage(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
 
+function crearUsuario() {
+  var username = document.getElementById("new-username").value;
+  var email = document.getElementById("new-email").value;
+  var password = document.getElementById("new-password").value;
+
+  const user = {
+    username: username,
+    email: email,
+    password: password,
+  };
+
+  document.getElementById("new-username").value = "";
+  document.getElementById("new-email").value = "";
+  document.getElementById("new-password").value = "";
+
+  localStorage.setItem(username, JSON.stringify(user));
+  alert("Usuario creado correctamente");
+}
+
+function iniciarSesion() {
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  let user = localStorage.getItem(username);
+  if (user) {
+    user = JSON.parse(user);
+    if (user.username == username && user.password == password) {
+      alert("Bienvenido " + user.username);
+      window.location.href = "index.html";
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  }
+}
