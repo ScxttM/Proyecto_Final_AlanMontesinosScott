0b42e50cb7f2258befab19ac5fe53dcf51d5c7ad
ScxttM <alan.montesinos@uabc.edu.mx>
2023-12-11 18:59:10 -0800

    avance

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-const template = (title = "hola") => String.raw`
-  <div>
-    <h1>${title}</h1>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-  `
-document.querySelector('#app').innerHTML = template("bsigfunbadson")
+
